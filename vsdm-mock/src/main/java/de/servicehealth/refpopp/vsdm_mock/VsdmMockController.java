4d77cb95c14b43487d5247a37a4ef0011670a519
MMQuintanilla <martaquint13@gmail.com>
2026-03-26 12:38:18 +0100

    removed target folder

--- vsdm-mock/src/main/java/de/servicehealth/refpopp/vsdm_mock/VsdmMockController.java
+++ vsdm-mock/src/main/java/de/servicehealth/refpopp/vsdm_mock/VsdmMockController.java
@@ -1,5 +1,26 @@
+/*
+ * Copyright (Date see Readme), gematik GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * *******
+ *
+ * For additional notes and disclaimer from gematik and in case of changes by gematik find details in the "Readme" file.
+ */
+
 package de.servicehealth.refpopp.vsdm_mock;
 
+import java.nio.charset.StandardCharsets;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.io.ClassPathResource;
@@ -8,29 +29,27 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.*;
 
-import java.nio.charset.StandardCharsets;
-
 @RestController
 @RequestMapping("/vsdm")
 public class VsdmMockController {
 
-    private static final Logger logger = LoggerFactory.getLogger(VsdmMockController.class);
+  private static final Logger logger = LoggerFactory.getLogger(VsdmMockController.class);
 
-    @GetMapping(value = "/bundle/{kvnr}", produces = MediaType.APPLICATION_XML_VALUE)
-    public ResponseEntity<String> getFhirBundle(
-            @PathVariable String kvnr,
-            @RequestHeader(value = "Authorization", required = false) String authorization) {
+  @GetMapping(value = "/bundle/{kvnr}", produces = MediaType.APPLICATION_XML_VALUE)
+  public ResponseEntity<String> getFhirBundle(
+      @PathVariable String kvnr,
+      @RequestHeader(value = "Authorization", required = false) String authorization) {
 
-        logger.info("Mock VSDM 2.0 backend called for KVNR: {} with token: {}", kvnr, authorization);
+    logger.info("Mock VSDM 2.0 backend called for KVNR: {} with token: {}", kvnr, authorization);
 
-        try {
-            ClassPathResource resource =
-                new ClassPathResource("fhir_bundle/019aa697-e026-7735-b898-09ead32a7fa5.xml");
-            byte[] data = FileCopyUtils.copyToByteArray(resource.getInputStream());
-            return ResponseEntity.ok(new String(data, StandardCharsets.UTF_8));
-        } catch (Exception e) {
-            logger.error("Error reading FHIR bundle from resources", e);
-            return ResponseEntity.internalServerError().build();
-        }
+    try {
+      ClassPathResource resource =
+          new ClassPathResource("fhir_bundle/019aa697-e026-7735-b898-09ead32a7fa5.xml");
+      byte[] data = FileCopyUtils.copyToByteArray(resource.getInputStream());
+      return ResponseEntity.ok(new String(data, StandardCharsets.UTF_8));
+    } catch (Exception e) {
+      logger.error("Error reading FHIR bundle from resources", e);
+      return ResponseEntity.internalServerError().build();
     }
+  }
 }
